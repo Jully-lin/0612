3fb7253b0fcd07ec0f275f1cd07c8c918f7b30fb
Jully-lin <61531538+Jully-lin@users.noreply.github.com>
2024-06-12 22:06:31 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,6 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("Hello!")
     basic.pause(5000)
     basic.showNumber(0)
-    basic.pause(100)
 })
 basic.forever(function () {
 	
