0b2e280a3e93146fe20cb49943ed521c18dfee9b
Jully-lin <61531538+Jully-lin@users.noreply.github.com>
2024-06-12 19:23:30 +0800

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def on_button_pressed_a():
     basic.show_string("Hello!")
+    basic.pause(5000)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_forever():
--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     basic.showString("Hello!")
+    basic.pause(5000)
 })
-basic.forever(function () {
+basic.forever(function on_forever() {
     basic.showNumber(0)
 })
