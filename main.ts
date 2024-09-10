28657376a0609270d91c1e174b31e79e433dc285
Jully-lin <61531538+Jully-lin@users.noreply.github.com>
2024-09-10 16:27:00 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showString("Hello!")
-    basic.pause(100)
-    led.plot(3, 0)
+basic.showIcon(IconNames.Heart)
+let a = 0
+basic.forever(function () {
+    for (let a = 0; a <= 255; a++) {
+        led.setBrightness(a)
+        basic.pause(2)
+    }
+    for (let a = 0; a <= 255; a++) {
+        led.setBrightness(255 - a)
+        basic.pause(2)
+    }
 })
